0a34eefc90294db32f20c0f2aba19c72b66122bf
islanderz <kamilns@hotmail.com>
2017-04-17 18:33:34 +0200

    Initial commit

---
+++ CMakeFiles/tum_ardrone_gencfg.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/tum_ardrone_gencfg"
+  "/home/kamil/catkin_ws/devel/include/tum_ardrone/AutopilotParamsConfig.h"
+  "/home/kamil/catkin_ws/devel/share/tum_ardrone/docs/AutopilotParamsConfig.dox"
+  "/home/kamil/catkin_ws/devel/share/tum_ardrone/docs/AutopilotParamsConfig-usage.dox"
+  "/home/kamil/catkin_ws/devel/lib/python2.7/dist-packages/tum_ardrone/cfg/AutopilotParamsConfig.py"
+  "/home/kamil/catkin_ws/devel/share/tum_ardrone/docs/AutopilotParamsConfig.wikidoc"
+  "/home/kamil/catkin_ws/devel/include/tum_ardrone/GUIParamsConfig.h"
+  "/home/kamil/catkin_ws/devel/share/tum_ardrone/docs/GUIParamsConfig.dox"
+  "/home/kamil/catkin_ws/devel/share/tum_ardrone/docs/GUIParamsConfig-usage.dox"
+  "/home/kamil/catkin_ws/devel/lib/python2.7/dist-packages/tum_ardrone/cfg/GUIParamsConfig.py"
+  "/home/kamil/catkin_ws/devel/share/tum_ardrone/docs/GUIParamsConfig.wikidoc"
+  "/home/kamil/catkin_ws/devel/include/tum_ardrone/StateestimationParamsConfig.h"
+  "/home/kamil/catkin_ws/devel/share/tum_ardrone/docs/StateestimationParamsConfig.dox"
+  "/home/kamil/catkin_ws/devel/share/tum_ardrone/docs/StateestimationParamsConfig-usage.dox"
+  "/home/kamil/catkin_ws/devel/lib/python2.7/dist-packages/tum_ardrone/cfg/StateestimationParamsConfig.py"
+  "/home/kamil/catkin_ws/devel/share/tum_ardrone/docs/StateestimationParamsConfig.wikidoc"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/tum_ardrone_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
